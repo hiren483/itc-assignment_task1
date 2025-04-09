942e6d8ae0ed82bf0a0a0a95d3b3ed0fc7293a95
hiren483 <hirenkavad99@gmail.com>
2025-04-09 11:12:36 +0100

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './components.css'
 import './home.css'
 
-const home = () => {
+const Home = () => {
   return (
     <>
       <div id='container1'>
@@ -12,24 +12,19 @@ const home = () => {
       </div>
 
       <div id='container2'>
-        <img src="/ITClogoWhite.png" alt="Vite Logo" id='itclogo' />
+        <img src={import.meta.env.BASE_URL + "ITClogoWhite.png"} alt="Vite Logo" id='itclogo' />
+
         <span id='itc_description'>
-
           <h1 id='aboutitc_text1'>About ITC</h1>
-
           <h2 id='aboutitc_text2'>Where tech meets action at IITB</h2>
 
           <p>
-
             The Institute Technical Council (ITC) is the heart of all things tech on campus. From guiding student innovations to organizing events, ITC drives the tech culture forward. It’s a space where ideas aren’t just discussed — they’re built. Whether you’re just starting out or already deep into projects, ITC’s got your back. We help you explore, experiment, and execute, turning wild ideas into real impact.
           </p>
-
         </span>
-
       </div>
-
     </>
   )
 }
 
-export default home
+export default Home
