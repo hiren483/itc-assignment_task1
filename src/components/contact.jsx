72396e505d0620e0012bb1d0c46af75b117ed658
hiren483 <hirenkavad99@gmail.com>
2025-04-09 20:43:30 +0100

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,7 +7,20 @@ import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg
 const Contact = () => {
   return (
     <div id='contact_container'>
-      <h1>feel free to reach at any time!</h1>
+
+      <div id='form-container'>
+      <form id="contact-form">
+        <input type="text" placeholder="Your Name" name="name" required className='form-field' />
+        <input type="email" placeholder="Your Email" name="email" required className='form-field'/>
+        <textarea placeholder="Your Message" name="message" rows="4" required className='form-field'/>
+        <button type="submit" className='form-field' id='submit-btn'>Send Message</button>
+      </form>
+
+
+      </div >
+
+      <div id='contact-links'>
+
       <ul>
         <li><FontAwesomeIcon icon={faFacebook} size="1x" /><a href="https://www.facebook.com/tech.iitb/" target="_blank" rel="noopener noreferrer">Facebook</a></li>
         <li><FontAwesomeIcon icon={faInstagram} size="1x" /> <a href="https://www.instagram.com/tech.iitb" target="_blank" rel="noopener noreferrer">instagram</a></li>
@@ -16,6 +29,7 @@ const Contact = () => {
         <li>📞8128679781</li>
         <li>✉️gsectech@iitb.ac.in</li>
       </ul>
+      </div>
     </div>
   )
 }
